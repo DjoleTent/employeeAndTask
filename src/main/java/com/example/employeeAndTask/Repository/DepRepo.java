773a1b4d3ad4b060f991edec55923517dcc4ea24
Djordje <djordjedjordjevic2@gmail.com>
2023-03-13 13:09:32 +0100

    forgot repo anotation...

--- src/main/java/com/example/employeeAndTask/Repository/DepRepo.java
+++ src/main/java/com/example/employeeAndTask/Repository/DepRepo.java
@@ -4,10 +4,12 @@ import com.example.employeeAndTask.model.Department;
 import com.example.employeeAndTask.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface DepRepo extends JpaRepository<Department, UUID> {
 
     List<Department> findAll();
